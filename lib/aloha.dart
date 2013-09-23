7ac517d08623da4cc5cee4d43b206d68a07075dc
Steve Hamblett <steve.hamblett@linux.com>
2013-09-23 13:00:15 +0100

    More structure

---
+++ lib/aloha.dart
@@ -0,0 +1,12 @@
+/*
+ * Packge : aloha
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 23/09/2013
+ * Copyright :  S.Hamblett@OSCF
+ */
+
+library aloha;
+
+import 'package:js/js.dart';
+
+part 'src/Aloha.dart';
